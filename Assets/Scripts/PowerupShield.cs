acc049e78e1163ec98d0352a7c544885b7dbe78a
aliwahaj393 <109057885+aliwahaj393@users.noreply.github.com>
2025-02-04 12:02:01 +0500

    Added TDM Scripts

---
+++ Assets/Scripts/PowerupShield.cs
@@ -0,0 +1,37 @@
+﻿namespace TanksMP
+{
+    /// <summary>
+    /// Custom powerup implementation for adding player shield points.
+    /// </summary>
+	public class PowerupShield : Collectible
+    {
+        /// <summary>
+        /// Amount of shield points to add per consumption.
+        /// </summary>
+        public int amount = 3;
+
+
+        /// <summary>
+        /// Overrides the default behavior with a custom implementation.
+        /// Check for the current shield and adds additional shield points.
+        /// </summary>
+		public override bool Apply(Player p)
+        {
+            if (p == null)
+                return false;
+
+            int value = p.GetView().GetShield();
+
+            //don't add shield if it is at the maximum already
+            if (value == amount)
+                return false;
+
+            //assign absolute shield points to player
+            //we can't go over the maximum thus no need to check it here
+            p.GetView().SetShield(amount);
+
+            //return successful collection
+            return true;
+        }
+    }
+}
